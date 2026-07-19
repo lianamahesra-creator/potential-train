e24f36d39e3bb275a87509dd1fc96d088b0101e5
lianamahesra-creator <lianamahesra@gmail.com>
2026-07-19 16:27:09 +0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# ============================================================
+#  run.sh — Mining commands
+#  Edit this file with your actual mining setup
+# ============================================================
+
+./forge \
+  --algorithm pearlhash \
+  --pool prl.kryptex.network:7048 \
+  --wallet prl1p2jan4dvkdfkt5r3pra7z96axrxjyjcgat9w7ldetlcy9wffm569sc9ux2t
